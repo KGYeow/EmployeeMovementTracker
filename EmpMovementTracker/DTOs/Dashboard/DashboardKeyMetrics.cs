4201c4db90665bc3910ac17b58e4a645a26febb9
KGYeow <kokguan0819@gmail.com>
2025-07-17 12:54:02 +0800

    Update

---
+++ EmpMovementTracker/DTOs/Dashboard/DashboardKeyMetrics.cs
@@ -0,0 +1,8 @@
+﻿namespace EmpMovementTracker.DTOs.Dashboard;
+
+public partial class DashboardKeyMetrics
+{
+    public int TotalEmployee { get; set; }
+    public int TotalTurnstileAccess { get; set; }
+    public string PeakHour { get; set; } = "N/A";
+}
--- EmpMovementTracker/DTOs/EmployeeTurnstileMovement/EmployeeMovementFilter.cs
+++ EmpMovementTracker/DTOs/EmployeeTurnstileMovement/EmployeeMovementFilter.cs
@@ -1,4 +1,4 @@
-﻿namespace EmpMovementTracker.DTOs;
+﻿namespace EmpMovementTracker.DTOs.EmployeeTurnstileMovement;
 
 public partial class EmployeeMovementFilter
 {
