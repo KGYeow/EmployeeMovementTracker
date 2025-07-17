47c94db7d8551ab3144d0437437e3b8429038051
shinichikudo1 <n.najihah1149@gmail.com>
2025-07-17 14:28:55 +0800

    Update the create page and uncommented code in timetracking

--- EmpMovementTracker/Services/EmployeeTimeTrackingService.cs
+++ EmpMovementTracker/Services/EmployeeTimeTrackingService.cs
@@ -28,43 +28,6 @@ namespace EmpMovementTracker.Services
             return await list.ToListAsync();
         }
 
-        //public async Task<List<EmployeeTimeTracking>> PresentEmployeeMovementList(EmployeeMovementFilter dto)
-        //{
-        //    var dateOnly = DateOnly.FromDateTime((DateTime)dto.Date);
-
-        //    var list = await context.EmployeeTimeTrackings
-        //        .Where(a => a.Date == dateOnly)
-        //        .ToListAsync();
-
-        //    // Apply filters
-        //    if (!string.IsNullOrEmpty(dto.Name))
-        //        list = list.Where(a => EF.Functions.Like(a.Name, $"%{dto.Name}%")).ToList();
-
-        //    if (!string.IsNullOrEmpty(dto.WorkCell))
-        //        list = list.Where(a => a.WorkCell == dto.WorkCell).ToList();
-
-        //    if (!string.IsNullOrEmpty(dto.Department))
-        //        list = list.Where(a => a.Department == dto.Department).ToList();
-
-        //    // Populate InitialTime and FinalTime from EmployeeMovements
-        //    foreach (var item in list)
-        //    {
-        //        var movements = await context.EmployeeMovements
-        //            .Where(m => m.PersonId == item.PersonId && m.Date == dateOnly)
-        //            .OrderBy(m => m.Time)
-        //            .ToListAsync();
-
-        //        if (movements.Any())
-        //        {
-        //            item.InitialTime = movements.First().Time;
-        //            item.FinalTime = movements.Last().Time;
-        //        }
-        //    }
-
-        //    return list;
-        //}
-
-
         // Get the employee info of an employee on a certain date.
         public async Task<EmployeeTimeTracking> EmployeeInfo(string personId, DateOnly date)
         {
