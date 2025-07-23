2525d5b4ae6609615da81b6ec31aa756fe21d195
KGYeow <kokguan0819@gmail.com>
2025-07-23 13:21:26 +0800

    Update

--- EmpMovementTracker/Services/DashboardService.cs
+++ EmpMovementTracker/Services/DashboardService.cs
@@ -12,9 +12,16 @@ namespace EmpMovementTracker.Services
         }
 
         // Get the key metrics data for based on the date.
-        public async Task<DashboardKeyMetrics> KeyMetricsData(DateOnly date)
+        public async Task<DashboardKeyMetrics> KeyMetricsData(DashboardFilter dto)
         {
-            var movements = await context.EmployeeMovements.Where(f => f.Date == date).ToListAsync();
+            if (dto.Date == null)
+                throw new ArgumentNullException(nameof(dto.Date), "Date is missing. Please select a date before applying the filter.");
+
+            var movements = await context.EmployeeMovements.Where(f => f.Date == DateOnly.FromDateTime((DateTime)dto.Date)).ToListAsync();
+
+            if (!string.IsNullOrEmpty(dto.BuildingId))
+                movements = movements.Where(a => a.BuildingId == dto.BuildingId).ToList();
+
             var peakHourGroup = movements.GroupBy(e => new TimeOnly(e.Time.Hour, e.Time.Minute, 0)).Select(g => new { g.Key.Hour, Count = g.Count() }).OrderByDescending(g => g.Count).FirstOrDefault();
 
             return new DashboardKeyMetrics
@@ -26,9 +33,15 @@ namespace EmpMovementTracker.Services
         }
 
         // Get the number of turnstile movement over time on a certain date.
-        public async Task<List<TimeSeriesChartSeries.TimeValue>> TurnstileActivityOverTime(DateOnly date)
+        public async Task<List<TimeSeriesChartSeries.TimeValue>> TurnstileActivityOverTime(DashboardFilter dto)
         {
-            var movements = await context.EmployeeMovements.Where(e => e.Date == date).Select(x => new { x.Id, x.Date, x.Time }).ToListAsync();
+            if (dto.Date == null)
+                throw new ArgumentNullException(nameof(dto.Date), "Date is missing. Please select a date before applying the filter.");
+
+            var movements = await context.EmployeeMovements.Where(e => e.Date == DateOnly.FromDateTime((DateTime)dto.Date)).Select(x => new { x.Id, x.Date, x.Time, x.BuildingId }).ToListAsync();
+
+            if (!string.IsNullOrEmpty(dto.BuildingId))
+                movements = movements.Where(a => a.BuildingId == dto.BuildingId).ToList();
 
             var chartData = movements
                 .GroupBy(e => new DateTime(e.Date.Year, e.Date.Month, e.Date.Day, e.Time.Hour, e.Time.Minute, 0))
@@ -39,9 +52,15 @@ namespace EmpMovementTracker.Services
         }
 
         // Get the number of turnstile movement for each building on a certain date.
-        public async Task<Dictionary<string, double>> TurnstileMovementEachBuilding(DateOnly date)
+        public async Task<Dictionary<string, double>> TurnstileMovementEachBuilding(DashboardFilter dto)
         {
-            var movements = await context.EmployeeMovements.Where(e => e.Date == date).Select(x => new { x.Id, x.BuildingId }).ToListAsync();
+            if (dto.Date == null)
+                throw new ArgumentNullException(nameof(dto.Date), "Date is missing. Please select a date before applying the filter.");
+
+            var movements = await context.EmployeeMovements.Where(e => e.Date == DateOnly.FromDateTime((DateTime)dto.Date)).Select(x => new { x.Id, x.BuildingId }).ToListAsync();
+
+            if (!string.IsNullOrEmpty(dto.BuildingId))
+                movements = movements.Where(a => a.BuildingId == dto.BuildingId).ToList();
 
             var chartData = movements
                 .GroupBy(e => e.BuildingId)
